bc6be0659c82803266b0139dfbca8528adb66ed0
rajanthetribe <107695766+rajanthetribe@users.noreply.github.com>
2022-10-10 16:11:19 +0530

    Custom Place Holder Screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -135,9 +135,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aff540b9213b1417df32af75d84b7185",
-"/": "aff540b9213b1417df32af75d84b7185",
-"main.dart.js": "dbab8d8737678df5c59a02d6fa9c8b7c",
+"index.html": "1d7e2efbce2a877d949be332a2a458ff",
+"/": "1d7e2efbce2a877d949be332a2a458ff",
+"main.dart.js": "cee2a31b76103acd85c5dc3e8bd84b28",
 "manifest.json": "8ef95d3397c7b63d7565e6eeac2754b4",
 "version.json": "e11da3a6143bf654a25e156a65061054"
 };
